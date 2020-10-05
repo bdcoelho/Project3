ea0d62986334310487b61ab1235223045afbbf25
bdcoelho <ben_coelho@hotmail.com>
2020-10-05 14:17:46 +1100

    repo cleanup

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-const {Client} = require("@googlemaps/google-maps-services-js");
-
-const client = new Client({});
-
-https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters
-
-https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters
-
-https://maps.googleapis.com/maps/api/place/autocomplete/json?input=1600+Amphitheatre&key=AIzaSyAwmiVLmIUNhiWqaGiGzlHl7WIec1ST8Ys
