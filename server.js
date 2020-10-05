1e6c4fc8e4cb55cc38d12fb17f43ddd64b3cff2c
bdcoelho <ben_coelho@hotmail.com>
2020-10-05 15:02:08 +1100

    heroku troubleshooting

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprses will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(
     session({ 
       secret: "keyboard cat", 
