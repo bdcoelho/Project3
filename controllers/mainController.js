cc6ca355660c2f0f5cd407559b7f9a9e995ddf4b
bdcoelho <ben_coelho@hotmail.com>
2020-10-08 15:30:46 +1100

    Serverside MVC refactor complete

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,9 @@
 const db = require("../models");
-const googleAPI = require("../routes/utils/googleAPI");
 const axios = require("axios");
+const googleAPI = require("./googleAPIController");
 let addressObject = {};
+let signUpObject = {};
+
 
 module.exports = {
   findNear: function (req, res) {
@@ -73,5 +75,48 @@ module.exports = {
       id: req.user.id,
     });
   },
+
+  logout: function (req, res) {
+    req.logout();
+    res.redirect("/");
+  },
+
+  signup: function (req, res) {
+
+    signUpObject.email = req.body.email,
+    signUpObject.password = req.body.password,
+    signUpObject.firstName = req.body.firstName,
+    signUpObject.lastName = req.body.lastName,
+    axios
+      .get(googleAPI.buildGeoCodeURL(req.body.address, "GeoCode"))
+      .then((response) => {
+        let resArray = response.data.results[0];
+        let coordsArray = [];
+        addressArray = resArray.address_components;
+        addressArray.forEach(googleAPI.parseGeoJSON, signUpObject);
+        coordsArray.push(
+          resArray.geometry.location.lng,
+          resArray.geometry.location.lat
+        );
+        console.log(coordsArray);
+        signUpObject.geometry = { type: "point", coordinates: coordsArray };
+        db.User.create(signUpObject)
+          .then(() => {
+            res.redirect(307, "/api/login");
+          })
+          .catch((err) => {
+            res.status(401).json(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+
+
+
+
+
+
 
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,7 @@
-const db = require("../models");
 const passport = require("../Auth/passport");
-const axios = require("axios");
-const googleAPI = require("./utils/googleAPI");
-const { parseGeoJSON } = require("./utils/googleAPI");
-const controller = require("../controllers/controller");
+const controller = require("../controllers/mainController");
 
-let signUpObject = {};
+
 
 module.exports = (app) => {
   // Login routing
@@ -14,42 +10,10 @@ module.exports = (app) => {
   app.post("/api/addressSearch", controller.addressSearch);
 
   // Signup routing
-  app.post("/api/signup", (req, res) => {
-    (signUpObject.email = req.body.email),
-      (signUpObject.password = req.body.password),
-      (signUpObject.firstName = req.body.firstName),
-      (signUpObject.lastName = req.body.lastName),
-      axios
-        .get(googleAPI.buildGeoCodeURL(req.body.address, "GeoCode"))
-        .then((response) => {
-          let resArray = response.data.results[0];
-          let coordsArray = [];
-          addressArray = resArray.address_components;
-          addressArray.forEach(googleAPI.parseGeoJSON, signUpObject);
-          coordsArray.push(
-            resArray.geometry.location.lng,
-            resArray.geometry.location.lat
-          );
-          console.log(coordsArray);
-          signUpObject.geometry = { type: "point", coordinates: coordsArray };
-          db.User.create(signUpObject)
-            .then(() => {
-              res.redirect(307, "/api/login");
-            })
-            .catch((err) => {
-              res.status(401).json(err);
-            });
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-  });
+  app.post("/api/signup", controller.signup);
 
   // Logging out routing
-  app.get("/api/logout", (req,res)=>{
-  req.logout();
-  res.redirect("/");
-  });
+  app.get("/api/logout", controller.logout);
 
   // getting user data if they are logged in
   app.get("/api/user_data", controller.getUserData);
