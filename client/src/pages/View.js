f2aaddbe62efe8f9f0dfd465dfedc2e24a4996b8
bdcoelho <ben_coelho@hotmail.com>
2020-10-15 10:18:43 +1100

    Fixed view page error

--- client/src/pages/View.js
+++ client/src/pages/View.js
@@ -6,56 +6,52 @@ import ViewCard from "../components/ViewCard";
 import axios from "axios";
 
 function View() {
-
   const [userAssets, setUserAssets] = useState([]);
   const { id, email, firstName, lastName } = useContext(UserContext);
   const retrieveAssets = () => {
-    axios
-      .get("/api/myAssets/" + id)
-      .then((res) => {
-        setUserAssets(res.data);
-      })
-      .catch((err) => console.log(err));
+    console.log(id);
+    if (id) {
+      axios
+        .get("/api/myAssets/" + id)
+        .then((res) => {
+          setUserAssets(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   useEffect(retrieveAssets, [id]);
 
   return (
     <div>
-    <Row>
-      <Col md={2} className="pr-0">
-        <SideNavBar />
-      </Col>
-      <Col md={9}>
-        <Row>
-          <CardDeck>
-            {userAssets.map((asset) => (
-
-
-              <Col md={4} key={asset._id}>
-                <ViewCard
-                
-                  name={asset.name}
-                  image={asset.image}
-                  category={asset.category}
-                  description={asset.description}
-                  hourly={asset.hourlyPrice}
-                  daily={asset.dailyPrice}
-                  id={asset._id}
-                  update={retrieveAssets}
-
-                />
-              </Col>
-            ))}
-          </CardDeck>
-        </Row>
-      </Col>
-    </Row>
-
-
-
-</div>
-
+      <Row>
+        <Col md={2} className="pr-0">
+          <SideNavBar />
+        </Col>
+        <Col md={9}>
+          <Row>
+            <CardDeck>
+              {userAssets.length > 0
+                ? userAssets.map((asset) => (
+                    <Col md={4} key={asset._id}>
+                      <ViewCard
+                        name={asset.name}
+                        image={asset.image}
+                        category={asset.category}
+                        description={asset.description}
+                        hourly={asset.hourlyPrice}
+                        daily={asset.dailyPrice}
+                        id={asset._id}
+                        update={retrieveAssets}
+                      />
+                    </Col>
+                  ))
+                : null}
+            </CardDeck>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
