70cf002ce315a7a7540c47171377378502bd6237
bdcoelho <ben_coelho@hotmail.com>
2020-10-14 19:20:57 +1100

    heroku testing

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Express serve up index.html file if it doesn't recognize route
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
